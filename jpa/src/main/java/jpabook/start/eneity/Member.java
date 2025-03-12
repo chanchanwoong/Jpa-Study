4871cd8b3d871fd4bd63ba2a96fb09369ecae82c
Chanwoong <cksdnddlek99@gmail.com>
2025-03-12 18:02:48 +0900

    feat: 구매 요구사항에 맞춰 개발 진행, 현재는 테이블 설계 위주로 코드가 작성(외래키가 엔티티가 아닌 필드 값으로 명시)

---
+++ jpa/src/main/java/jpabook/start/eneity/Member.java
@@ -0,0 +1,67 @@
+package jpabook.start.eneity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="members")
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String city, street, zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
+    }
+}
